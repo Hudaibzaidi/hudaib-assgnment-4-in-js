5fa8d3308fd7ce9a656560978bdf7b24d2d972db
Hudaibzaidi <117076704+Hudaibzaidi@users.noreply.github.com>
2022-11-14 17:38:29 +0500

    hudaib assgnment 4 in js
    hudaib assgnment 4 in js

---
+++ app.js
@@ -0,0 +1,94 @@
+    //chapter 1 
+    alert("enter your code")
+
+
+    alert(" hello world \n my using alert")
+    
+    
+    alert("enetr your passward")
+    
+    alert("my google \n enter your passward")
+    
+    alert("Hello...Ican run js thorught my web brower concole")
+    
+    alert(" my brower")
+        //chapter 1 is compelete
+    
+    
+        //chapter 2
+    var username ;
+    var myname =("syed hudaib hussain zaidi")
+    
+    var massege = "my name is hudaib hussain" 
+    var title = "hello world"
+    
+    alert(title)
+    alert(myname)
+    
+    var years ="18 years old"
+    alert(years)
+    
+    var catified ="catified mobile application devalopment"
+    alert(catified)
+    
+    var price ;
+    alert("Pizza\n Pizz \n Piz \n Pi \n p")
+    
+    var email1 = "zaidihudaib8@gmail.com"
+    alert("my email is " + email1)
+    
+    var book2 = "“A smarter \n way to learn JavaScript”."
+    alert("I am trying to learn from the book " + book2)
+    
+    
+    document.write("yeah I can write html content throught javascript")
+    
+    var daigram ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+    alert(daigram)
+            //chapter 2 is compelete
+    
+        //chapter 3
+    alert("my age "+years)
+    
+    var visited4 = "“You have visited this site 11 times”."
+    alert(visited4)
+    
+    document.write("my birth is 2002 data type of my declered variable is number ")
+    
+    var visiterdName = "syed hudaib"
+    var productName = "T-shirt"
+    var qontitinate = "6 T shirt"
+    
+    document.write(visiterdName + " ordered " + qontitinate +"(s) on XYZ Clothing store.")
+        //chapter 3 is compalate
+    
+        //chapter 4
+    var room ="hudaib hussain"
+    var bulb ="hudaib hussain"
+    var light ="hudaib hussain"
+    
+    var door1 = "legal variable"
+    var door$ = "legal variable"
+    var door_one = "legal variable"
+    var doorOne = "legal variable"
+    var door = "legal variable"
+    
+    //var phone-one ="illegal"
+    //var phon@two ="illagal"
+    //var phone&three ="illegal"
+    //var phone*four ="illegal"
+    //var 5phone ="illegal"
+    
+    document.write(" <h1>Rules for naming JS variables</h1>")
+    
+    var name1 = "$my_1stVariable" ;
+    var name2 = "$ and" ;
+    document.write(" Variable names can only contain ," + name2 + " For example " + name1)
+    
+    document.write(" Variables must begin with a or. For example $name, _name or name")
+    var romio1 ="sensitive"
+    document.write(" Variable names are case " + romio1)
+    var romio ="keyword"
+    document.write(" Variable names should not be JS " + romio)
+     //chapter 4 is compalate
+    
